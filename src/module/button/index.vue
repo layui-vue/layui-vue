663cb908b532885c75727d23cc1560129a4ed5da
就眠仪式 <854085467@qq.com>
2021-09-27 11:27:36 +0800

    [更新] README.md

--- src/module/button/index.vue
+++ src/module/button/index.vue
@@ -1,15 +1,18 @@
 <template>
-    <button class="layui-btn" :class="[type ? 'layui-btn-' + type : '',size ? 'layui-btn-' + size : '']">
-        <slot></slot>
-    </button>
+  <button
+    class="layui-btn"
+    :class="[type ? 'layui-btn-' + type : '', size ? 'layui-btn-' + size : '']"
+  >
+    <slot></slot>
+  </button>
 </template>
 
 <script setup name="LayButton" lang="ts">
-import { defineProps } from "@vue/runtime-core";
+import { defineProps } from '@vue/runtime-core'
 
-const props = defineProps<{
-  type?: string,
-  size?: string
-}>()
-
+const props =
+  defineProps<{
+    type?: string
+    size?: string
+  }>()
 </script>
