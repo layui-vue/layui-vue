87168bbb0064a1aa4723edd48780b3af47f7579d
就眠仪式 <854085467@qq.com>
2021-10-16 23:41:35 +0800

    perf(button): 新增 loading 属性, 支持加载状态

--- src/module/carousel/index.vue
+++ src/module/carousel/index.vue
@@ -4,39 +4,60 @@
     lay-anim
     lay-indicator="inside"
     lay-arrow="always"
-    :style="{width:width,height:height}"
+    :style="{ width: width, height: height }"
   >
     <div carousel-item>
-      <div> </div>
-      <div> </div>
-      <div> </div>
-      <div> </div>
-      <div class="layui-this"></div>
+      <slot></slot>
     </div>
     <div class="layui-carousel-ind">
       <ul>
-        <li class=""></li>
-        <li class=""></li>
-        <li class=""></li>
-        <li class=""></li>
-        <li class="layui-this"></li>
+        <li
+          v-for="ss in slots"
+          :key="ss"
+          :class="[ss.props.id === modelValue ? 'layui-this' : '']"
+          @click.stop="change(ss.props.id)"
+        ></li>
       </ul>
     </div>
-    <button class="layui-icon layui-carousel-arrow" lay-type="sub"></button
-    ><button class="layui-icon layui-carousel-arrow" lay-type="add"></button>
+    <button class="layui-icon layui-carousel-arrow" lay-type="sub" @click="prev"></button
+    ><button class="layui-icon layui-carousel-arrow" lay-type="add" @click="next"></button>
   </div>
 </template>
 <script setup name="LayCarousel" lang="ts">
-import { withDefaults, defineProps } from 'vue'
+import { withDefaults, defineProps, provide, useSlots, ref } from 'vue'
+
+const slot = useSlots() as any
+const slots = slot.default && slot.default()
 
 const props = withDefaults(
   defineProps<{
-    width: string,
-    height: string
+    width?: string
+    height?: string
+    modelValue: string
   }>(),
   {
     width: '100%',
-    height: '280px'
+    height: '280px',
   }
 )
+
+const active = ref(props.modelValue)
+
+const emit = defineEmits(['update:modelValue','change','close'])
+
+const change = function (id: any) {
+  emit('update:modelValue', id)
+  emit('change', id)
+  active.value = id
+}
+
+provide('active', active)
+
+const prev = function() {
+    console.log("上一页")
+}
+
+const next = function() {
+    console.log("下一页")
+}
 </script>
