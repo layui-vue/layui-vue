de92a5fd2e2cf83a4bd307ff61c448d025d3b28a
就眠儀式 <854085467@qq.com>
2022-01-29 23:07:16 +0800

    更新 exception 组件

--- example/docs/zh-CN/components/exception.md
+++ example/docs/zh-CN/components/exception.md
@@ -1,6 +1,34 @@
 ::: anchor
 :::
 
+::: title 401
+:::
+
+::: demo 使用 `lay-exception` 标签, 创建一个异常页面
+
+<template>
+  <lay-exception status="401" title="401" describe="暂无相关权限">
+      <template #action>
+        <lay-button>刷新</lay-button>
+        <lay-button type="primary">返回</lay-button>
+    </template>
+  </lay-exception>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+
+    return {
+    }
+  }
+}
+</script>
+
+:::
+
 ::: title 403
 :::
 
--- src/component/exception/index.vue
+++ src/component/exception/index.vue
@@ -1,4 +1,5 @@
 <script lang="ts">
+import { useSlots } from '@vue/runtime-core';
 export default {
     name: "LayException"
 }
@@ -8,22 +9,27 @@ import "./index.less";
 
 export interface LayDropdownProps {
   title?: string,
-  status?: '403' | '404' | '500',
+  status?: '401' | '403' | '404' | '500',
   describe?: string
 }
+
+const slot = useSlots();
 
 const props = withDefaults(defineProps<LayDropdownProps>(), {
   title: "Exception",
   describe: "describe"
 });
-
 </script>
 <template>
     <div class="layui-exception">
         <div class="layui-exception-image">
-            <div v-if="status=='403'" class="error-403" />
-            <div v-if="status=='404'" class="error-404" />
-            <div v-if="status=='500'" class="error-500" />
+            <slot name="image" v-if="slot.default"></slot>
+            <template v-else>
+                <div v-if="status=='401'" class="error-401" />
+                <div v-if="status=='403'" class="error-403" />
+                <div v-if="status=='404'" class="error-404" />
+                <div v-if="status=='500'" class="error-500" />
+            </template>
         </div>
         <div class="layui-exception-details">
             <div class="layui-exception-details-content">
