7457d9b6541d9755bccbaaba9b4f4d220a42f89c
鄢鹏权 <490523604@qq.com>
2022-02-10 10:38:46 +0000

    [移除] 删除多余的console，该console影响开发正常的console内容

--- src/component/tree/tree.ts
+++ src/component/tree/tree.ts
@@ -135,7 +135,7 @@ class Tree {
   setChildrenChecked(checked: boolean, nodes: TreeData[]) {
     const len = nodes.length;
     for (let i = 0; i < len; i++) {
-      console.log(nodes[i], checked);
+      //console.log(nodes[i], checked);
       nodes[i].isChecked.value = checked;
       nodes[i].children &&
         nodes[i].children.length > 0 &&
@@ -168,7 +168,7 @@ class Tree {
     if (node.children) {
       this.setChildrenChecked(checked, node.children);
     }
-    console.log(this.getData());
+    //console.log(this.getData());
   }
 
   getData() {
