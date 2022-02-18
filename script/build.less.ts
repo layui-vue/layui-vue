818790ad278ab040a6bece693d83bbfbb11efb03
就眠儀式 <854085467@qq.com>
2022-02-18 09:39:37 +0800

    docs: 更新日志

--- script/build.less.ts
+++ script/build.less.ts
@@ -5,19 +5,19 @@
 var fs = require('fs');
 var { resolve } = require('path');
 
+// @ts-ignore
 const inputDir = resolve(process.cwd(), './src/component')
 
+// @ts-ignore
 const inputsArray = fs.readdirSync(inputDir).filter((name) => {
   const componentDir = resolve(inputDir, name)
   const isDir = fs.lstatSync(componentDir).isDirectory()
   return isDir && fs.readdirSync(componentDir).includes('index.ts')
 })
 
-// 读取基础变量 与 公共样式
 var content = fs.readFileSync("./src/theme/variable.less").toString() + '\n\n';
 content += fs.readFileSync("./src/theme/index.less").toString() + '\n\n';
 
-// 组件样式
 inputsArray.forEach(function (f) {
   var path = "./src/component/" + f + "/index.less";
   fs.exists(path, function (exists) {
