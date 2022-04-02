34f2c9d992a6b321c053997fe294808b821b51b9
sight <1453017105@qq.com>
2022-04-02 19:06:38 +0800

    feat(timeline): 横向排列

--- src/component/timeline/index.vue
+++ src/component/timeline/index.vue
@@ -6,10 +6,24 @@ export default {
 
 <script setup lang="ts">
 import "./index.less";
+import { computed, withDefaults } from "vue";
+
+export interface LayTimelineProps {
+  direction: "horizontal" | "vertical";
+}
+
+const props = withDefaults(defineProps<LayTimelineProps>(), {
+  direction: "vertical",
+});
+
+const timeLineClass = computed(() => [
+  "layui-timeline",
+  props.direction === "horizontal" ? "layui-timeline-horizontal" : "",
+])
 </script>
 
 <template>
-  <ul class="layui-timeline">
+  <ul :class="timeLineClass">
     <slot></slot>
   </ul>
 </template>
