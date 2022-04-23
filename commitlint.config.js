0b79aa072b6604b1667df0c7b2e828b7c7caa779
就眠儀式 <854085467@qq.com>
2022-04-24 00:36:00 +0800

    🔧 工具: 集成 cz-customizable 依赖

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,3 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-enum": [2, 
-      "always",
-      [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test'
-      ]
-    ],
-  },
+  extends: ["cz"],
 };
