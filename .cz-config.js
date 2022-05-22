539af99222b0a38616e681f67610a38c3113f75a
就眠儀式 <854085467@qq.com>
2022-05-22 13:14:05 +0800

    🔧: 增加 commitlint 可选项

--- .cz-config.js
+++ .cz-config.js
@@ -6,10 +6,13 @@ module.exports = {
     { value: "📝", name: "文档:    变更的只有文档" },
     { value: "💄", name: "格式:    空格, 分号等格式修复" },
     { value: "♻️", name: "重构:    代码重构，注意和特性、修复区分开" },
+    { value: "🌀", name: "样式:    样式的调整" },
     { value: "⚡️", name: "性能:    提升性能" },
     { value: "✅", name: "测试:    添加一个测试" },
     { value: "🔧", name: "工具:    开发工具变动(构建、脚手架工具等)" },
     { value: "⏪", name: "回滚:    代码回退" },
+    { value: "⬆️", name: "升级:    依赖升级" },
+    { value: "⬇️", name: "降级:    依赖降级" },
   ],
   scopes: [
     { name: "component" },
