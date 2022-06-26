802e6696d01877f14cb2b42a7305909381ca13fb
sight <1453017105@qq.com>
2022-06-26 17:32:47 +0800

    ✨: playground hash gzip

--- package/document-component/src/composable/usePlayground.ts
+++ package/document-component/src/composable/usePlayground.ts
@@ -1,4 +1,5 @@
 import type { BuiltInParserName } from "prettier";
+import { zlibSync, unzlibSync, strToU8, strFromU8 } from "fflate";
 
 const scriptRe = /<script[^>]*>([\s\S]*)<\/script>/;
 const exportDefaultRe = /export\s*default\s*\{([\s\S]*)\}\;?\s*<\/script>/;
@@ -78,24 +79,6 @@ export const usePlayGround = async (
 };
 
 /**
- * 编码
- * @param data
- * @returns
- */
-function utoa(data: string): string {
-  return btoa(unescape(encodeURIComponent(data)));
-}
-
-/**
- * 去除字符串两端的空白行
- * @param str
- * @returns
- */
-function trimBr(str: string): string {
-  return str.replace(/(^[\r\n]*)|([\r\n]*$)/, "");
-}
-
-/**
  *
  * @returns 格式化代码
  */
@@ -130,4 +113,35 @@ async function formatCode(filename: string, data: string) {
   });
 
   return code;
+}
+
+/**
+ * 去除字符串两端的空白行
+ * @param str
+ * @returns
+ */
+function trimBr(str: string): string {
+  return str.replace(/(^[\r\n]*)|([\r\n]*$)/, "");
+}
+
+export function utoa(data: string): string {
+  const buffer = strToU8(data);
+  const zipped = zlibSync(buffer, { level: 9 });
+  const binary = strFromU8(zipped, true);
+  return btoa(binary);
+}
+
+export function atou(base64: string): string {
+  const binary = atob(base64);
+
+  // zlib header (x78), level 9 (xDA)
+  if (binary.startsWith("\x78\xDA")) {
+    const buffer = strToU8(binary, true);
+    const unzipped = unzlibSync(buffer);
+    return strFromU8(unzipped);
+  }
+
+  // old unicode hacks for backward compatibility
+  // https://base64.guru/developers/javascript/examples/unicode-strings
+  return decodeURIComponent(escape(binary));
 }
