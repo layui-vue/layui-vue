c09f4766b897c24cafa92f37517f41fe47f37e5f
就眠儀式 <854085467@qq.com>
2022-07-10 12:04:43 +0800

    ✨: 发布 1.2.8 版本

--- package/document-component/src/view/index.vue
+++ package/document-component/src/view/index.vue
@@ -43,7 +43,7 @@
             rel="nofollow"
             class="site-star"
           >
-            <i class="layui-icon"></i> Star <cite id="getStars">1272</cite>
+            <i class="layui-icon"></i> Star <cite id="getStars">1296</cite>
           </a>
           <a
             href="https://gitee.com/layui-vue"
