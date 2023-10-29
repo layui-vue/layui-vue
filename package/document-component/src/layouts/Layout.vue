ec856072f6037426b19f35fe507e94d5fda1629f
就眠儀式 <854085467@qq.com>
2023-10-30 02:42:15 +0800

    ✨(component): update
    ISSUES CLOSED: ‘

--- package/document-component/src/layouts/Layout.vue
+++ package/document-component/src/layouts/Layout.vue
@@ -121,8 +121,8 @@
           <li class="layui-nav-item">
             <a href="javascript:void(0)">
               <lay-tag
-              size="xs"
-              class="layui-local-badge"
+                size="xs"
+                class="layui-local-badge"
                 v-if="locale === 'en_US'"
                 @click="changeLocale('zh_CN')"
                 >中 文</lay-tag>
@@ -398,9 +398,22 @@ export default {
 }
 
 .layui-header .layui-local-badge {
-  font-size: 12.4px;
   background: transparent;
+  line-height: 20px;
+  height: 20px;
 }
+
+.layui-badge-rim {
+  border-radius: 2px;
+  border: 1px solid whitesmoke;
+  font-size: 12px;
+  padding: 3px 10px;
+}
+
+.layui-header .layui-local-badge .layui-tag-text {
+  font-size: 12px;
+}
+
 
 .layui-menu-docs {
   padding-top: 10px;
