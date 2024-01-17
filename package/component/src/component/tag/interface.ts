360afb0f6012496c514bb7516b67a45ea93d977d
Jevin <cwj_8888@163.com>
2024-01-17 19:35:33 +0800

    🧹: update chore emoji

--- package/component/src/component/tag/interface.ts
+++ package/component/src/component/tag/interface.ts
@@ -1,6 +1,6 @@
 export const TAG_COLORS = ["primary", "normal", "warm", "danger"] as const;
 
-export type TagType = (typeof TAG_COLORS)[number];
+export type TagType = typeof TAG_COLORS[number];
 
 export type TagShape = "square" | "round";
 
