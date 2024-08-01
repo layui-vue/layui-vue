63592d4651e35115d39cc76c2f023b28670b2943
Jevin <cwj_8888@163.com>
2024-08-01 13:16:12 +0000

    !374 fix(layer): 修复umd格式下globalIndex无默认值 * fix(layer): 修复umd格式下globalIndex无默认值

--- packages/layer/src/tokens/index.ts
+++ packages/layer/src/tokens/index.ts
@@ -10,7 +10,8 @@ export const nextIndex = function () {
   if ((window as any).layer.globalIndex == -1) {
     (window as any).layer.globalIndex = inject(zIndexKey, 99999);
   } else {
-    (window as any).layer.globalIndex = (window as any).layer.globalIndex + 1;
+    (window as any).layer.globalIndex =
+      ((window as any).layer.globalIndex || 99999) + 1;
   }
   return (window as any).layer.globalIndex;
 };
