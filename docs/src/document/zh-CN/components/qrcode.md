e808a625cc640bde7c7d3fbf6ff092763f336ccf
SpikeMeow <5637753+colisys@user.noreply.gitee.com>
2024-08-17 10:21:57 +0800

    feat: layQrcode 组件支持内插图片

--- docs/src/document/zh-CN/components/qrcode.md
+++ docs/src/document/zh-CN/components/qrcode.md
@@ -76,6 +76,40 @@ export default {
 
 :::
 
+::: title 设置图片
+:::
+
+::: demo 通过 `images` 属性，设置二维码中插入的图片。
+
+<template>
+  <lay-qrcode text="http://www.layui-vue.com" :images="images"></lay-qrcode>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const images = ref([
+      {
+        image: 'https://www.layui-vue.com/assets/logo-png-B_GrttYE.png',
+        width: 40,
+        height: 40,
+        top: 0,
+        left: 0,
+        background: false,
+      }, // 画到左上角
+      "https://www.layui-vue.com/assets/logo-png-B_GrttYE.png" // 自动画到正中间
+    ])
+    return {
+      images,
+    }
+  }
+}
+</script>
+
+:::
+
 ::: title Qrcode 属性
 :::
 
@@ -83,11 +117,29 @@ export default {
 
 | 属性         | 描述     | 类型    | 默认值    | 可选值                             | 版本    |
 | ------------ | -------- | ------- | --------- | ---------------------------------- | ------- |
-| text         | 内容     | string  | --  | --  | --      |
-| width        | 宽度     | number  | --        | --                    | --      |
-| color        | 颜色     | string | --    | --                    | --      |
-| background-color       | 背景色     | string | --    | --                      | --      |
-| options       | QRCode其他参数     | object | --    | --                      | --      |
+| *text*         | 内容     | `string`  | --  | --  | --      |
+| *width*        | 宽度     | `number`  | --        | --                    | --      |
+| *color*        | 颜色     | `string` | --    | --                    | --      |
+| *backgroundColor*       | 背景色     | `string` | --    | --                      | --      |
+| *options*       | QRCode其他参数     | `Record<string, any>` | --    | --                      | --      |
+| *images*        | 图片     | `string`  `Array<string>` `Array<QrcodeImageProps>` | --    | --                      | --      |
+
+:::
+
+::: title QrcodeImageProps
+:::
+
+::: table
+
+| 属性         | 描述     | 类型    | 默认值    | 可选值                             | 版本    |
+| ------------ | -------- | ------- | --------- | ---------------------------------- | ------- |
+| *image*       | 图片地址     | `string`  | --  | --  | --      |
+| *width*       | 宽度     | `number`  | --        | --                    | --      |
+| *height*      | 高度     | `number`  | --    | --                    | --      |
+| *top*         | y轴     | `number`  | --    | --                      | --      |
+| *left*        | x轴     | `number`  | --    | --                      | --      |
+| *background*  | 启用背景 | `boolean` | `true`    | --                      | --      |
+| *backgroundColor*       | 背景色     | `string` | --    | --                      | --      |
 
 :::
 
--- packages/component/component/qrcode/index.vue
+++ packages/component/component/qrcode/index.vue
@@ -9,12 +9,24 @@ import { ref, onMounted, nextTick, watch } from "vue";
 import QRCode from "qrcode";
 import "./index.less";
 
+export interface QrcodeImageProps {
+  image: string;
+  left?: number;
+  top?: number;
+  width?: number;
+  height?: number;
+  size?: number;
+  background?: boolean;
+  backgroundColor?: string;
+}
+
 export interface QrcodeProps {
   text: string;
   backgroundColor?: string;
   width?: number;
   color?: string;
   options?: Record<string, any>;
+  images?: Array<QrcodeImageProps> | Array<string> | string;
 }
 
 defineOptions({
@@ -38,6 +50,57 @@ watch(
   { deep: true }
 );
 
+const drawImage = () => {
+  if (!props.images?.length) return;
+  nextTick(() => {
+    const ctx = (qrcode.value as HTMLCanvasElement)?.getContext("2d");
+    const images = Array.isArray(props.images) ? props.images : [props.images];
+    if (!ctx) return;
+    images
+      .filter((i) => i !== undefined)
+      ?.forEach((i) => {
+        let src = typeof i === "string" ? i : i.image;
+        let width, height, top, left, size, background, backgroundColor;
+        width = typeof i === "string" ? 32 : i.width;
+        height = typeof i === "string" ? 32 : i.height;
+        top = typeof i === "string" ? undefined : i.top;
+        left = typeof i === "string" ? undefined : i.left;
+        size = typeof i === "string" ? 32 : i.size;
+        background = typeof i === "string" ? true : i.background;
+        backgroundColor =
+          typeof i === "string" ? props.backgroundColor : i.backgroundColor;
+
+        if (size) {
+          width = size;
+          height = size;
+        }
+
+        if (width === undefined) width = 32;
+        if (height === undefined) height = 32;
+        if (top === undefined) top = ctx.canvas.height / 2 - height / 2;
+        if (left === undefined) left = ctx.canvas.width / 2 - width / 2;
+        if (background === undefined) background = true;
+        if (backgroundColor === undefined) backgroundColor = "#fff";
+
+        const el = document.createElement("img");
+        el.src = src;
+        el.onload = () => {
+          if (background) {
+            ctx.fillStyle = backgroundColor;
+            ctx.fillRect(left, top, width, height);
+          }
+          ctx.drawImage(
+            el,
+            left, // src left
+            top, // src top
+            width, // src width
+            height // src height
+          );
+        };
+      });
+  });
+};
+
 const buildQrcode = () => {
   QRCode.toCanvas(qrcode.value, props.text, {
     width: props.width,
@@ -46,6 +109,8 @@ const buildQrcode = () => {
       light: props.backgroundColor,
     },
     ...props.options,
+  }).then(() => {
+    drawImage();
   });
 };
 </script>
