1a23256d32e4cbb7a021afd808f19785eee79773
Jevin <cwj_8888@163.com>
2024-08-22 11:29:21 +0800

    chore: code optimization

--- packages/component/component/qrcode/index.vue
+++ packages/component/component/qrcode/index.vue
@@ -6,6 +6,7 @@
 
 <script lang="ts" setup>
 import { ref, onMounted, nextTick, watch } from "vue";
+import { isString } from "@vue/shared";
 import QRCode from "qrcode";
 import "./index.less";
 
@@ -54,50 +55,42 @@ const drawImage = () => {
   if (!props.images?.length) return;
   nextTick(() => {
     const ctx = (qrcode.value as HTMLCanvasElement)?.getContext("2d");
-    const images = Array.isArray(props.images) ? props.images : [props.images];
     if (!ctx) return;
-    images
-      .filter((i) => i !== undefined)
-      ?.forEach((i) => {
-        let src = typeof i === "string" ? i : i.image;
-        let width, height, top, left, size, background, backgroundColor;
-        width = typeof i === "string" ? 32 : i.width;
-        height = typeof i === "string" ? 32 : i.height;
-        top = typeof i === "string" ? undefined : i.top;
-        left = typeof i === "string" ? undefined : i.left;
-        size = typeof i === "string" ? 32 : i.size;
-        background = typeof i === "string" ? true : i.background;
-        backgroundColor =
-          typeof i === "string" ? props.backgroundColor : i.backgroundColor;
 
-        if (size) {
-          width = size;
-          height = size;
+    const images = Array.isArray(props.images) ? props.images : [props.images as string];
+    images.forEach((i: QrcodeImageProps | string) => {
+      let {
+        image,
+        width = 32,
+        height = 32,
+        top = ctx.canvas.height / 2 - height / 2,
+        left = ctx.canvas.width / 2 - width / 2,
+        size = 32,
+        background = true,
+        backgroundColor = "#fff"
+      } = isString(i) ? { image: i } : (i as QrcodeImageProps);
+
+      if (size) {
+        width = size;
+        height = size;
+      }
+
+      const el = document.createElement("img");
+      el.src = image;
+      el.onload = () => {
+        if (background) {
+          ctx.fillStyle = backgroundColor;
+          ctx.fillRect(left, top, width, height);
         }
-
-        if (width === undefined) width = 32;
-        if (height === undefined) height = 32;
-        if (top === undefined) top = ctx.canvas.height / 2 - height / 2;
-        if (left === undefined) left = ctx.canvas.width / 2 - width / 2;
-        if (background === undefined) background = true;
-        if (backgroundColor === undefined) backgroundColor = "#fff";
-
-        const el = document.createElement("img");
-        el.src = src;
-        el.onload = () => {
-          if (background) {
-            ctx.fillStyle = backgroundColor;
-            ctx.fillRect(left, top, width, height);
-          }
-          ctx.drawImage(
-            el,
-            left, // src left
-            top, // src top
-            width, // src width
-            height // src height
-          );
-        };
-      });
+        ctx.drawImage(
+          el,
+          left, // src left
+          top, // src top
+          width, // src width
+          height // src height
+        );
+      };
+    });
   });
 };
 
