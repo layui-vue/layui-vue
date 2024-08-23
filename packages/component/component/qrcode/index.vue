f8f0e5ddea7688e26e2337f2afe8014379511bd2
SpikeMeow <5637753+colisys@user.noreply.gitee.com>
2024-08-23 01:19:55 +0000

    !382  feat(layQrcode ): 支持内插图片 * fix: typescript 类型适配 * chore: eslint 和文档补充 * chore: code optimization * feat: layQrcode 组件支持内插图片

--- packages/component/component/qrcode/index.vue
+++ packages/component/component/qrcode/index.vue
@@ -6,8 +6,20 @@
 
 <script lang="ts" setup>
 import { ref, onMounted, nextTick, watch } from "vue";
+import { isString } from "@vue/shared";
 import QRCode from "qrcode";
 import "./index.less";
+
+export interface QrcodeImageProps {
+  image: string;
+  left?: number;
+  top?: number;
+  width?: number;
+  height?: number;
+  size?: number;
+  background?: boolean;
+  backgroundColor?: string;
+}
 
 export interface QrcodeProps {
   text: string;
@@ -15,6 +27,7 @@ export interface QrcodeProps {
   width?: number;
   color?: string;
   options?: Record<string, any>;
+  images?: Array<QrcodeImageProps> | Array<string> | string;
 }
 
 defineOptions({
@@ -38,6 +51,53 @@ watch(
   { deep: true }
 );
 
+const drawImage = () => {
+  if (!props.images?.length) return;
+  nextTick(() => {
+    const ctx = (qrcode.value as HTMLCanvasElement)?.getContext("2d");
+    if (!ctx) return;
+
+    const images = Array.isArray(props.images)
+      ? props.images
+      : [props.images as string];
+    images.forEach((i: QrcodeImageProps | string) => {
+      let {
+        image,
+        width = 32,
+        height = 32,
+        top = ctx.canvas.height / 2 - height / 2,
+        left = ctx.canvas.width / 2 - width / 2,
+        size = 32,
+        background = true,
+        backgroundColor = "#fff",
+      }: QrcodeImageProps = isString(i)
+        ? { image: i }
+        : (i as QrcodeImageProps);
+
+      if (size) {
+        width = size;
+        height = size;
+      }
+
+      const el = document.createElement("img");
+      el.src = image;
+      el.onload = () => {
+        if (background) {
+          ctx.fillStyle = backgroundColor;
+          ctx.fillRect(left, top, width, height);
+        }
+        ctx.drawImage(
+          el,
+          left, // src left
+          top, // src top
+          width, // src width
+          height // src height
+        );
+      };
+    });
+  });
+};
+
 const buildQrcode = () => {
   QRCode.toCanvas(qrcode.value, props.text, {
     width: props.width,
@@ -46,6 +106,8 @@ const buildQrcode = () => {
       light: props.backgroundColor,
     },
     ...props.options,
+  }).then(() => {
+    drawImage();
   });
 };
 </script>
