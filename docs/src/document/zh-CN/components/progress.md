93cc7313b22a46e924d54d010256bcae9b26cded
SpikeMeow <5637753+colisys@user.noreply.gitee.com>
2024-09-20 23:28:25 +0800

    feat: layProgress 进度支持分段和动画

--- docs/src/document/zh-CN/components/progress.md
+++ docs/src/document/zh-CN/components/progress.md
@@ -32,6 +32,71 @@ export default {
 
 :::
 
+::: title 动画
+:::
+
+::: demo 使用 `animated` 开启动画。**不可与 `indeterminate` 共用。** 默认在进度达到 100% 时停止动画，你可以指定 `full-animated` 属性来保持动画。
+
+<template>
+  <div style="display: flex; flex-direction: column; gap: 8px;">
+    <LayProgress animated :percent="50"></LayProgress>
+    <LayProgress animated :percent="100"></LayProgress>
+    <LayProgress animated full-animated :percent="100"></LayProgress>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+</script>
+
+:::
+
+::: title 无限进度条
+:::
+
+::: demo 使用 `indeterminate` 属性创建无限进度条。**不可与 `animated` 共用。**
+
+<template>
+  <LayProgress indeterminate></LayProgress>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+</script>
+
+:::
+
+::: title 进度分段
+:::
+
+::: demo 提供 `Array<PorgressLinearProps>` 类型到 `precent` 属性上，可以创建进度分段。
+
+<template>
+  <LaySpace direction="vertical" style="width: 100%">
+    <LayProgress animated :percent="percent"></LayProgress>
+  </LaySpace>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const percent = ref([
+      { percent: 50, theme: "green", showText: true, text: "123" },
+      { percent: 20, theme: "info", showText: true, text: "4" },
+      { percent: 10, theme: "orange", showText: true, text: "5" },
+    ]);
+
+    return {
+      percent,
+    };
+  },
+};
+</script>
+
+:::
+
 ::: title 尺寸大小
 :::
 
@@ -145,6 +210,7 @@ export default {
   }
 }
 </script>
+
 :::
 
 ::: title Progress 属性
@@ -152,17 +218,21 @@ export default {
 
 ::: table
 
-| 属性     | 描述     | 可选值                                        |
-| -------- | -------- | --------------------------------------------- |
-| percent  | 进度     | --                                            |
-| theme    | 主题     | `orange` `green` `cyan` `blue` `black` `gray` |
-| size     | 尺寸     | `big`                                         |
-| text     | 提示     | --                                            |
-| color    | 颜色     | --                                            |
-| showText | 展示描述  | --                                            |
-| circle    | 环形进度条| 默认为 `false`                                 |
-| circleSize| 环形进度条尺寸| 默认为 `150` 单位是px                        |
-| circleWidth| 环形进度条线条宽度| 默认为 `6` 单位是px                       |
+| 属性           | 描述               | 类型              | 可选值                                        | 默认值  | 版本     |
+| -------------- | ------------------ | ----------------- | --------------------------------------------- | ------- | -------- |
+| percent        | 进度               | `number` `string` | --                                            |         |          |
+| theme          | 主题               | `string`          | `orange` `green` `cyan` `blue` `black` `gray` |         |          |
+| size           | 尺寸               | `string`          | `big`                                         |         |          |
+| text           | 提示               | `string`          | --                                            |         |          |
+| color          | 颜色               | `string`          | --                                            |         |          |
+| show-text      | 展示描述           | `boolean`         | --                                            |         |          |
+| circle         | 环形进度条         | `boolean`         |                                               | `false` |          |
+| circle-size    | 环形进度条尺寸     | `number`          | 默认为 `150` 单位是 px                        | `150`   |          |
+| circle-width   | 环形进度条线条宽度 | `number`          | 默认为 `6` 单位是 px                          | `6`     |          |
+| animated       | 是否开启动画       | `boolean`         | --                                            | `false` | `2.19.0` |
+| full-animated  | 是否开启全动画     | `boolean`         | --                                            | `false` | `2.19.0` |
+| indeeterminate | 是否为无限进度条   | `boolean`         | --                                            | `false` | `2.19.0` |
+
 :::
 
 ::: previousNext progress
--- packages/component/component/progress/index.vue
+++ packages/component/component/progress/index.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+import { isArray } from "../../utils";
 import "./index.less";
-import { computed } from "vue";
+import { computed, ComputedRef, ref, watch } from "vue";
+
+export type ProgressLineProps = {
+  percent: number | string;
+  theme?: string;
+  color?: string;
+  showText?: boolean;
+  text?: string;
+};
 
 export interface ProgressProps {
-  percent: number | string;
+  percent: number | string | Array<ProgressLineProps>;
   theme?: string;
   color?: string;
   size?: string;
@@ -12,6 +21,9 @@ export interface ProgressProps {
   circle?: boolean;
   circleSize?: number;
   circleWidth?: number;
+  animated?: boolean;
+  fullAnimated?: boolean;
+  indeterminate?: boolean;
 }
 
 defineOptions({
@@ -24,64 +36,95 @@ const props = withDefaults(defineProps<ProgressProps>(), {
   circleWidth: 6,
 });
 
-const styles = computed(() => {
-  return [
-    props.color ? "background-color: " + props.color : "",
-    {
-      width: props.percent + "%",
-    },
-  ];
+const getColorByTheme = (theme?: string) => {
+  switch (theme) {
+    case "red":
+      return "#ff5722";
+    case "orange":
+      return "#ffb800";
+    case "green":
+      return "#009688";
+    case "cyan":
+      return "#2f4056";
+    case "blue":
+      return "#1e9fff";
+    case "black":
+      return "#393d49";
+    case "gray":
+      return "#fafafa";
+    default:
+      return "#5FB878";
+  }
+};
+
+watch(
+  () => props.percent,
+  () => normalizePercent(),
+  { deep: true }
+);
+
+const normalizePercent = () => {
+  const base = {
+    percent: 0,
+    theme: props.theme,
+    color: props.color,
+    showText: props.showText,
+    text: props.text,
+  };
+
+  if (props.indeterminate)
+    return [
+      {
+        ...base,
+        percent: 100,
+      },
+    ];
+
+  return isArray(props.percent)
+    ? props.percent.map((i) => {
+        return {
+          ...base,
+          ...i,
+          percent: Number(i.percent),
+        };
+      })
+    : [
+        {
+          ...base,
+          percent: Number(props.percent),
+        },
+      ];
+};
+
+const percents = computed(() => normalizePercent());
+
+const percentsTotal = computed(() =>
+  percents.value.map((i) => i.percent).reduce((a, b) => a + b, 0)
+);
+
+const percentMaxWidth = computed(() => `${percentsTotal.value}%`);
+
+const linearStyles = computed(() => {
+  return percents.value.map((i, v) => {
+    const sum = percents.value
+      .map((i) => Number(i.percent))
+      .slice(0, v)
+      .reduce((a, b) => a + b, 0);
+    return {
+      left: `${sum}%`,
+      width: `${100 - sum > i.percent ? i.percent : 100 - sum}%`,
+      backgroundColor: i.color ?? getColorByTheme(i.theme),
+    };
+  });
 });
 
 const getCircleColor = computed(() => {
-  let color;
-  switch (props.theme) {
-    case "red":
-      color = "#ff5722";
-      break;
-    case "orange":
-      color = "#ffb800";
-      break;
-    case "green":
-      color = "#009688";
-      break;
-    case "cyan":
-      color = "#2f4056";
-      break;
-    case "blue":
-      color = "#1e9fff";
-      break;
-    case "black":
-      color = "#393d49";
-      break;
-    case "gray":
-      color = "#fafafa";
-      break;
-    default:
-      color = "#5FB878";
-      break;
-  }
-  color = props.color ? props.color : color;
-  return color;
-});
-
-const getCircleRotate = computed(() => {
-  let percent;
-  if (typeof props.percent == "string") {
-    percent = parseInt(props.percent);
-  } else {
-    percent = props.percent;
-  }
-  return (percent / 100) * 360;
+  return props.color ?? getColorByTheme(props.theme);
 });
 
 const getStrokeDasharray = computed(() => {
-  let percent;
-  if (typeof props.percent == "string") {
-    percent = parseInt(props.percent);
-  } else {
-    percent = props.percent;
-  }
+  if (isArray(props.percent)) return;
+  let percent = Number(props.percent);
   let radii = props.circleSize / 2 - props.circleWidth / 2;
   let perimeter = Math.PI * 2 * radii;
   return `${(percent / 100) * perimeter}px ${perimeter}px`;
@@ -126,16 +169,104 @@ const getPathD = computed(() => {
       {{ text ? text : percent + "%" }}
     </div>
   </div>
-
-  <div class="layui-progress" :class="'layui-progress-' + size" v-else>
-    <div
-      class="layui-progress-bar"
-      :class="'layui-bg-' + theme"
-      :style="styles"
-    >
-      <span v-if="showText" class="layui-progress-text">
-        {{ text ? text : percent + "%" }}
-      </span>
+  <div
+    class="layui-progress"
+    :class="[
+      'layui-progress-' + size,
+      {
+        'layui-progress-animated':
+          animated && !indeterminate && (percentsTotal < 100 || fullAnimated),
+      },
+      { 'layui-progress-indeterminate': indeterminate },
+    ]"
+    v-else
+  >
+    <div class="layui-progress-bar-container">
+      <div
+        class="layui-progress-bar"
+        :style="linearStyles.at(v)"
+        v-for="(i, v) in percents"
+        :key="v"
+      >
+        <span v-if="i.showText" class="layui-progress-text">
+          {{ i.text ? i.text : i.percent + "%" }}
+        </span>
+      </div>
     </div>
   </div>
 </template>
+
+<style>
+.layui-progress-animated::after {
+  content: "";
+  position: absolute;
+  background-color: #fff;
+  height: 100%;
+  width: v-bind(percentMaxWidth);
+  border-radius: var(--progress-bar-border-radius);
+  animation: progressMove 2s ease-in-out infinite;
+}
+
+.layui-progress-indeterminate::after {
+  content: "";
+  position: absolute;
+  background-color: #fff;
+  opacity: 0.8;
+  height: 100%;
+  border-radius: calc(var(--progress-bar-border-radius) * -1);
+  animation: progressIndeterminate 3s linear infinite;
+}
+
+@keyframes progressMove {
+  0% {
+    width: 0;
+    opacity: 0;
+  }
+
+  50% {
+    opacity: 0.5;
+  }
+
+  100% {
+    width: v-bind(percentMaxWidth);
+    opacity: 0;
+  }
+}
+
+@keyframes progressIndeterminate {
+  0% {
+    left: 0;
+    width: 0;
+  }
+
+  20% {
+    left: 50%;
+    width: 50%;
+  }
+
+  40% {
+    left: 100%;
+    width: 0;
+  }
+
+  50% {
+    left: 0;
+    width: 0;
+  }
+
+  60% {
+    left: 0%;
+    width: 100%;
+  }
+
+  80% {
+    left: 60%;
+    width: 40%;
+  }
+
+  100% {
+    left: 100%;
+    width: 0%;
+  }
+}
+</style>
