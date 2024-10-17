040c620ec0b7920a66a9b38e0d44d3a116cf80dd
SpikeMeow <5637753+colisys@user.noreply.gitee.com>
2024-10-17 11:03:07 +0000

    !420 feat(component/Layer): 支持服务调用时给 header 和 footer 传入 VNode * docs: update * chore: 目录调整 * Merge branch 'jevin/layer-fix-interactive-slot-render-fork' of https:/… * test: add more test * chore: ts type * chore: update * chore: update * chore: 增加文档和代码修复 * feat(component/Layer): 支持服务调用时给 header 和 footer 传入 VNode

---
+++ packages/layer/src/component/__tests__/layer.test.tsx
@@ -0,0 +1,62 @@
+import { nextTick, h, defineComponent } from "vue";
+import { afterEach, describe, expect, it, test } from "vitest";
+import { layer } from "../../index";
+import { sleep } from "../../../../component/test-utils";
+
+const CustomComponent = defineComponent({
+  setup(props, ctx) {
+    return () => h("div", { class: "custom-footer" }, ctx.slots);
+  },
+});
+
+describe("Layer", () => {
+  afterEach(async () => {
+    layer.closeAll();
+    await nextTick();
+    await sleep();
+  });
+
+  it("should work", async () => {
+    const open = () => {
+      layer.open({
+        title: "Hello",
+      });
+    };
+
+    open();
+    await nextTick();
+
+    const dom = document.querySelector(
+      ".layui-layer .layui-layer-title"
+    ) as HTMLDivElement;
+    expect(dom).toBeTruthy();
+    expect(dom.innerText).toBe("Hello");
+  });
+
+  test("layer.open 支持title/footer渲染组件", async () => {
+    const open = () => {
+      layer.open({
+        type: 1,
+        title: () =>
+          h(CustomComponent, { class: "custom-title" }, () => "title"),
+        footer: () =>
+          h(CustomComponent, { class: "custom-footer" }, () => "footer"),
+        content: "内容",
+      });
+    };
+
+    open();
+
+    await nextTick();
+
+    const titleDom = document.querySelector(
+      ".layui-layer .layui-layer-title .custom-title"
+    ) as HTMLDivElement;
+    const footerDom = document.querySelector(
+      ".layui-layer .layui-layer-footer .custom-footer"
+    ) as HTMLDivElement;
+
+    expect(titleDom.innerText).toBe("title");
+    expect(footerDom.innerText).toBe("footer");
+  });
+});
