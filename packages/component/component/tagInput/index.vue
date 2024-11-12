c69bde4907397de1622be8ebc6e9951af3db21b7
Jevin <cwj_8888@163.com>
2024-11-12 09:48:08 +0800

    fix(tagInput): 初始化inputValue未反填至输入框

--- packages/component/component/tagInput/index.vue
+++ packages/component/component/tagInput/index.vue
@@ -311,6 +311,10 @@ onMounted(() => {
   handleResize();
   const treeSelectContext = useTreeSelectProvide();
   treeSelectContext?.setInputEl?.(inputRefEl.value as HTMLInputElement);
+
+  if (inputRefEl.value) {
+    inputRefEl.value!.value = inputValue.value;
+  }
 });
 
 defineExpose({
