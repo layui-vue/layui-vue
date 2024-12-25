72127e45be497d78dc56898dab70295cff237f54
Jevin <cwj_8888@163.com>
2024-12-25 16:29:08 +0800

    chore(play): 改变文件后终端刷新打印Url入口

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import HmrRefreshUrlLog from "./plugins/vite-plugin-hmr-refresh-url-log";
 import "./vite.init";
 
 export default defineConfig(() => {
   return {
-    plugins: [vue(), vueJsx()],
+    plugins: [vue(), vueJsx(), HmrRefreshUrlLog()],
     server: {
       host: true,
       open: true,
