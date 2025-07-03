232e57c97458a03d8b3ae0db49767246b58d8c24
SpikeMeow <5637753+colisys@user.noreply.gitee.com>
2025-07-03 23:57:14 +0800

    feat(chat): 多媒体消息支持

--- packages/component/component/chat/index.vue
+++ packages/component/component/chat/index.vue
@@ -1,13 +1,24 @@
 <script lang="ts" setup>
 import type { Message } from "./interface";
 import { computed, nextTick, ref, watch } from "vue";
+import { select } from "../../utils";
+import LayAvatar from "../avatar/index";
+import LayButton from "../button/index";
+import LayCol from "../col/index";
+import LayEmpty from "../empty/index";
+import LayIcon from "../icon/index";
+import LayInput from "../input/index";
+import LayRow from "../row/index";
+import LayScroll from "../scroll/index";
+import LayTooltip from "../tooltip/index";
 import "./index.less";
 
 export interface ChatProps {
-  contentHeight: string | number;
+  contentHeight?: string | number;
   title?: string;
   messages?: Message[];
   backgroundColor?: string;
+  emptyText?: string;
 };
 
 defineOptions({
@@ -20,12 +31,13 @@ const props = withDefaults(
     contentHeight: 300,
     messages: () => [],
     title: undefined,
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
+    emptyText: "无消息",
   },
 );
 
 const emits = defineEmits<{
-  (e: "beforeSend", message: Message): boolean | void;
+  (e: "before-send", message: Message): boolean | void;
   (e: "sent", message: Message): boolean | void;
 }>();
 
@@ -45,7 +57,7 @@ function handleSendClick() {
       direction: "out",
     };
     chatInput.value = "";
-    if (emits("beforeSend", msg) === false)
+    if (emits("before-send", msg) === false)
       return;
     messageMap.set(msg.id, msg);
     messages.value.push(msg);
@@ -59,7 +71,11 @@ watch(
     nextTick(() => {
       if (messageScroll.value) {
         const el = (messageScroll.value as any).scrollRef as HTMLElement;
-        el.scrollTo(0, el.scrollHeight);
+        el.scrollTo({
+          behavior: "smooth",
+          top: el.scrollHeight,
+          left: 0,
+        });
       }
     });
   },
@@ -74,6 +90,18 @@ defineExpose({
     messageMap.set(msg.id, msg);
   },
   exportMessages: () => JSON.stringify(messages.value),
+  searchMessage: (keyword: RegExp) => messages.value.filter(
+    m => select(
+      () => [typeof m.content === "string" && keyword.test(`${m.content}`), m.content],
+      () => [keyword.test(`${m.name}`), m.name],
+      () => [keyword.test(`${m.id}`), m.id],
+    ),
+  ),
+  stash: (id: string | number = 0) => {
+    const msgs = messages.value.splice(messageMap.has(id) ? messages.value.findIndex(m => m.id === id) : 0);
+    nextTick();
+    return JSON.stringify(msgs);
+  },
 });
 </script>
 
@@ -90,9 +118,9 @@ defineExpose({
       <div class="layui-chat-content">
         <LayCol :md="24" class="layui-chat-content-wrapper" style="position: relative;">
           <template v-if="!messages.length">
-            <div style="inset: 0; display: flex; align-items: center; justify-content: center; position: absolute;">
+            <div class="layui-chat-content-empty">
               <slot name="empty">
-                <LayEmpty />
+                <LayEmpty :description="props.emptyText" />
               </slot>
             </div>
           </template>
@@ -103,11 +131,52 @@ defineExpose({
               ]">
                 <div class="layui-chat-content-item-wrapper">
                   <div v-if="message.direction !== 'system'" style="width: 32px; height: 32px;">
-                    <LayAvatar :src="message.avatar ?? ''" />
+                    <LayTooltip :content="message.name ?? ''">
+                      <LayAvatar :src="message.avatar ?? ''" />
+                    </LayTooltip>
                   </div>
-                  <span class="layui-chat-content-message">
-                    {{ message.content }}
-                  </span>
+                  <template v-if="message.type === 'text'">
+                    <template v-if="message.direction !== 'system'">
+                      <slot name="text-content" v-bind="{ message }">
+                        <span class="layui-chat-content-message" :style="{
+                          color: message.textColor,
+                          backgroundColor: message.backgroundColor,
+                        }">
+                          {{ message.content }}
+                        </span>
+                      </slot>
+                    </template>
+                    <template v-if="message.direction === 'system'">
+                      <slot name="system-content" v-bind="{ message }">
+                        <span class="layui-chat-content-message" :style="{
+                          color: message.textColor,
+                          backgroundColor: message.backgroundColor,
+                        }">
+                          {{ message.content }}
+                        </span>
+                      </slot>
+                    </template>
+                  </template>
+                  <template v-if="message.type === 'image'">
+                    <slot name="image-content" v-bind="{ message }">
+                      <img :src="message.content as string" class="layui-chat-content-image">
+                    </slot>
+                  </template>
+                  <template v-if="message.type === 'video'">
+                    <slot name="video-content" v-bind="{ message }">
+                      <video :src="message.content as string" controls class="layui-chat-content-video" />
+                    </slot>
+                  </template>
+                  <template v-if="message.type === 'audio'">
+                    <slot name="audio-content" v-bind="{ message }">
+                      <audio :src="message.content as string" controls class="layui-chat-content-audio" />
+                    </slot>
+                  </template>
+                  <template v-if="message.type === 'custom'">
+                    <slot name="custom-content" v-bind="{ message }">
+                      <span class="layui-chat-content-message">暂不支持该消息</span>
+                    </slot>
+                  </template>
                   <LayIcon v-if="message.direction !== 'system' && message.icon" :type="message.icon" />
                 </div>
               </div>
@@ -117,8 +186,8 @@ defineExpose({
       </div>
       <div class="layui-chat-input">
         <LayCol :md="24">
-          <LayInput @keydown="(ev: KeyboardEvent) => ev.keyCode === 13 && handleSendClick()" v-model="chatInput"
-            placeholder="Input..." class="layui-chat-input-box">
+          <LayInput v-model="chatInput" placeholder="Input..." class="layui-chat-input-box"
+            @keydown="(ev: KeyboardEvent) => ev.keyCode === 13 && handleSendClick()">
             <template #prefix>
               <slot name="shortcuts" />
             </template>
--- packages/component/utils/common.ts
+++ packages/component/utils/common.ts
@@ -1,3 +1,16 @@
+import type { Ref } from "vue";
+
 const CLASS_PREFIX_NAME = "layui";
 
 export const clsPrefix = (cls: string) => `${CLASS_PREFIX_NAME}-${cls}`;
+export function select(...branches: Array<() => [boolean, any]>) {
+  return branches.map(branch => branch()).find(([a, _]) => a);
+}
+export function delayCompare(source: Ref<any>, delay: number): Promise<[boolean, any, any]> {
+  return new Promise((resolve) => {
+    const that = source.value;
+    setTimeout(() => {
+      resolve([source.value === that, source.value, that]);
+    }, delay);
+  });
+}
